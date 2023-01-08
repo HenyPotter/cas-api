bbd10b790286cb56c2a1f62e3076b515b793d574
Jakub Holub <holubkuba2007@gmail.com>
2023-01-08 13:03:22 +0100

    nekam se posouvam...

--- index.js
+++ index.js
@@ -3,31 +3,25 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 async function scrapeTable() {
-  // Make a request to the webpage
   const response = await axios.get('https://online.atletika.cz/Propozice/propozice/66181');
 
-  // Load the HTML into cheerio
   const $ = cheerio.load(response.data);
-
-  // Select the table using the element selector
   const table = $('table.timeTable.table-striped');
 
-  // Initialize an empty array to store the data
-  const data = [];
+  const data = {};
 
-  // Loop through each row in the table
   table.find('tr').each((i, row) => {
-    // Initialize an empty array to store the row data
-    const rowData = [];
 
-    // Loop through each cell in the row
-    $(row).find('td').each((j, cell) => {
-      // Push the cell data to the row data array
-      rowData.push($(cell).text().trim());
-    });
+    const rowData = {};
 
-    // Push the row data to the overall data array
-    data.push(rowData);
+
+    const center = $(row).find('td.center').text().trim().replace(/ {6}/g, '');
+    const disciplineTypeAtTime = $(row).find('span.disciplineTypeAtTime').text().trim();
+
+    rowData['time'] = center;
+    rowData['discipline'] = disciplineTypeAtTime;
+
+    data[i] = rowData;
   });
 
   // Return the data
@@ -35,6 +29,6 @@ async function scrapeTable() {
 }
 
 scrapeTable().then(data => {
-  // Write the data to the file, replacing \n characters with an empty string
-  fs.writeFileSync('table.json', JSON.stringify(data).originalString.replace(/\\n                                                                /g, ''));
+  // Write the data to the file
+  fs.writeFileSync('table.json', JSON.stringify(data).replace(/\\n                                                                /g, ''));
 });
