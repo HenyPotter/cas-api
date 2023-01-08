5f5f21f86a293c9e50fdf5a0eea6f92b3a8e5cf5
Jakub Holub <holubkuba2007@gmail.com>
2023-01-08 12:02:37 +0100

    juchu funguje neco

--- index.js
+++ index.js
@@ -3,20 +3,38 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 async function scrapeTable() {
+  // Make a request to the webpage
   const response = await axios.get('https://online.atletika.cz/Propozice/propozice/66181');
+
+  // Load the HTML into cheerio
   const $ = cheerio.load(response.data);
-  const table = $('table.table-striped');
+
+  // Select the table using the element selector
+  const table = $('table.timeTable.table-striped');
+
+  // Initialize an empty array to store the data
   const data = [];
+
+  // Loop through each row in the table
   table.find('tr').each((i, row) => {
+    // Initialize an empty array to store the row data
     const rowData = [];
-    $(row).find('span.disciplineTypeAtTime').each((j, cell) => {
+
+    // Loop through each cell in the row
+    $(row).find('td').each((j, cell) => {
+      // Push the cell data to the row data array
       rowData.push($(cell).text().trim());
     });
+
+    // Push the row data to the overall data array
     data.push(rowData);
   });
+
+  // Return the data
   return data;
 }
 
 scrapeTable().then(data => {
-  fs.writeFileSync('table.json', JSON.stringify(data));
+  // Write the data to the file, replacing \n characters with an empty string
+  fs.writeFileSync('table.json', JSON.stringify(data).originalString.replace(/\\n                                                                /g, ''));
 });
